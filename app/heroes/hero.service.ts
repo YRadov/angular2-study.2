cf754c32aedf0c03cffdc6635b033b6a8ae5d49d
YRadov-ПК\YRadov <radov.yuriy@ukr.net>
2016-10-13 22:03:56 +0300

    end DI

--- app/heroes/hero.service.ts
+++ app/heroes/hero.service.ts
@@ -5,10 +5,23 @@ import { Logger }     from '../logger.service';
 
 @Injectable()
 export class HeroService {
+
     constructor(private logger: Logger) {  }
 
     getHeroes() { 
         this.logger.log('Getting heroes ...');
         return HEROES; 
     }
+
+    // constructor(
+    //   private logger: Logger,
+    //   private isAuthorized: boolean
+    //   ) { }
+    
+    // getHeroes() {
+    //   let auth = this.isAuthorized ? 'authorized ' : 'unauthorized';
+    //   this.logger.log(`Getting heroes for ${auth} user.`);
+    //   return HEROES.filter(hero => this.isAuthorized || !hero.isSecret);
+    // }
+    
 }
