8a22d98121ef2b37c09924fd80c63255ce18b200
YRadov-ПК\YRadov <radov.yuriy@ukr.net>
2016-10-13 21:00:54 +0300

    add Logger service

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,11 +2,12 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
 
-import { AppComponent }  from './app.component';
+import { AppComponent }      from './app.component';
 import { HeroFormComponent } from './hero-form.component';
-import { HeroesComponent } from './heroes/heroes.component';
+import { HeroesComponent }   from './heroes/heroes.component';
 import { HeroListComponent } from './heroes/hero-list.component';
-//import { HeroService } from './heroes/hero.service';
+//import { HeroService }     from './heroes/hero.service';
+import { Logger }            from './logger.service';
 
 @NgModule({
   imports: [
@@ -21,6 +22,7 @@ import { HeroListComponent } from './heroes/hero-list.component';
   ],
   providers: [
     //HeroService,
+    Logger
   ],
   bootstrap: [ AppComponent ]
 })
