f2c4f1bcb470c20d6d55b8044071cef643fe4202
YRadov-ПК\YRadov <radov.yuriy@ukr.net>
2016-10-13 17:43:39 +0300

    add DI in constructor

--- app/car/car.ts
+++ app/car/car.ts
@@ -2,12 +2,16 @@ export class Car {
 
   public engine: Engine;
   public tires: Tires;
-  public description = 'No DI';
+//   public description = 'No DI';
 
-  constructor() {
-    this.engine = new Engine();
-    this.tires = new Tires();
-  }
+//   constructor() {
+//     this.engine = new Engine();
+//     this.tires = new Tires();
+//   }
+
+public description = 'DI';
+
+constructor(public engine: Engine, public tires: Tires) { }
   
   // Method using the engine and tires
   drive() {
